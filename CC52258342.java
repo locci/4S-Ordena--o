d70c9f87c3ad16e95b0d6af1ab6d0052a765bf14
Kyllex <kylmax@hotmail.com>
2016-03-16 14:08:20 -0300

    Ep04
    Sobre Orientação a Objetos

---
+++ CC52258342.java
@@ -0,0 +1,108 @@
+package ep4;
+
+	/**
+	 * Classe Microondas Padrão
+	 * @author Kelvy
+	 *
+	 */
+
+public class CC52258342 {
+	
+	int tempoMaxCosimento = 100;
+	int potenciaMax = 100;
+	
+	int descongelar(String value){
+		return 1;
+	}	
+
+	int cozinhar(String value){
+		return 1;
+	}
+
+	int timer(String value){
+		return 1;
+		
+	}
+	
+	int pipoca(String value){
+		return 1;
+		
+	}
+	
+	int cha(String value){
+		return 1;
+		
+	}
+	
+	int grelhar(String value){
+		return 1;
+	}
+}
+
+	/**
+	 * Classe Potente
+	 * @author Kelvy
+	 *
+	 */
+
+ 	class MicroondasPotente extends CC52258342 {
+	
+	
+	
+	int cozimentoAlto(String value){
+		return 1;		
+	}
+
+}
+ 	interface Potenciador {
+ 		
+ 		int superDescongelante(String value);
+ 		int superTostado(String value);
+
+ 	}
+	
+ 	
+
+ 	/**
+ 	 * Classe Economico
+ 	 * @author Kelvy
+ 	 *
+ 	 */
+
+ 	class MicroondasEconomico extends AbstractMicroondasEconomico{
+	
+	int cozimentoBaixo(String value){
+		return 1;
+		
+	}
+
+	@Override
+	int pesarPeixe(String value) {
+		return 1;
+	}
+	
+}
+	/**
+	 * Classe AbstractEconomico
+	 * @author Kelvy
+	 *
+	 */
+ 	abstract class AbstractMicroondasEconomico extends CC52258342{
+ 			
+ 		abstract int pesarPeixe(String value);
+ 		
+ 		int pesarCarne(String value){
+ 			return 1;
+ 			
+ 		}
+ 		int pesarAves(String value){
+ 			return 1;
+ 			
+ 		}
+}
+
+ 
+ 
+ 
+ 
+ 
